677b7412f9b1e87f92e04c374322facdfd55450a
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-02 12:49:55 -0400

    update recsession controller

--- app/controllers/recsessions_controller.rb
+++ app/controllers/recsessions_controller.rb
@@ -1,2 +1,48 @@
 class RecSessionsController < ApplicationController
+  before_action :set_recsession, except: [:new, :create]
+  def index
+    # @recsessions = List of all Users recsessions
+  end
+
+  def new
+    @recsession = RecSession.new
+  end
+
+ 
+  def create
+    recsession = RecSession.new(recsession_params)
+    if recsession.valid? 
+      recsession.save
+      redirect_to recsession_path(recsession)
+    else
+      flash[:alert] = "Make sure to fill out all fieilds."
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+  
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def recsession_params
+    params.require(:recsession).permit(:name, :address, :daw, :type)
+  end
+
+  def set_recsession
+    @recsession = recsession.find(params[:id])
+  end
 end
