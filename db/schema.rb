69ff90e0f1698b9e099d1eb2082def4f39e47ca3
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-02 11:49:51 -0400

    migrate Studio table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_153045) do
+ActiveRecord::Schema.define(version: 2021_08_02_154449) do
+
+  create_table "studios", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "daw"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
