b4be7209171cc2cb414db350ec418780e193234b
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-02 12:15:00 -0400

    update studio controller

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,2 +1,48 @@
 class StudiosController < ApplicationController
+  before_action :set_studio, except: [:new, :create]
+  def index
+    # @studios = List of all Users Studios
+  end
+
+  def new
+    @studio = Studio.new
+  end
+
+ 
+  def create
+    studio = Studio.new(studio_params)
+    if studio.valid? 
+      studio.save
+      redirect_to studio_path(studio)
+    else
+      flash[:alert] = "Make sure to fill out all fieilds."
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+  
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def studio_params
+    params.require(:studio).permit(:name, :address, :daw, :type)
+  end
+
+  def set_studio
+    @studio = Studio.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :studios
-  resources :users, except: [:index, :delete]
-  resources :studio
+  resources :users, except: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
