6d725847ca5634941351101f002aa320d58b7f66
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-02 12:01:22 -0400

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
 
-#   def new
-#   end
+  def new
+  end
 
-#   def create
-#     user = User
-#   end
+  def create
+    user = User.new(user_params)
+    if user.valid? 
+      user.save
+      session[:user_id] = user.id
+      redirect_to 'home'
+    else
+      flash[:alert] = "User needs a name, password, and a unique email."
+      render :new
+    end
+  end
 end
