4e5b1ba7969401d6a5442996a9445518757091b8
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-02 13:44:43 -0400

    remove client routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :clients
   resources :studios do
-  resources :sessions, shallow: true
+    resources :sessions, shallow: true
   end
   resources :users, except: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
