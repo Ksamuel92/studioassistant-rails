e51522e695835b3a63fe64acef381fca2290e239
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-02 12:43:13 -0400

    add nested session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :sessions
-  resources :studios
+  resources :studios do
+  resources :sessions, shallow: true
+  end
   resources :users, except: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
