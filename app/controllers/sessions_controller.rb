ad68c8f0fa5b242706eb6785668db0b05f899a9a
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 11:29:37 -0400

    build out sessions#create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,20 @@
 class SessionsController < ApplicationController
+  
   def omniauth
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.valid?
-        session[:user_id] = user.id
-        redirect_to user_path(user)
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     else
-        redirect_to '/login'
+      redirect_to '/login'
     end
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    return render 'new' unless @user&.authenticate(params[:password])
+
+    sessions[:user_id] = @user.id
+    redirect_to '/'
   end
 end
