fdb5c629ecfa798ab0c7686de44487241503c375
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 17:06:01 -0400

    add require_login and current_user helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  def require_login
+    unless current_user
+      redirect_to '/'
+      flash[:alert] = "You must login to view that page"
+    end
+  end
+
+  def current_user
+    @user = User.find_by(id: session[:user_id])
+  end
 end
