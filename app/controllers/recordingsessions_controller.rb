a51d5cd6e58b2deb96f532f950dfa56698474a3c
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 13:05:34 -0400

    update recordingsessions controller

---
+++ app/controllers/recordingsessions_controller.rb
@@ -0,0 +1,45 @@
+class RecordingSessionsController < ApplicationController
+  before_action :set_recordingsession, except: [:new, :create]
+  def index
+    # @recordingsessions = List of all Users recordingsessions
+  end
+
+  def new
+    @recordingsession = RecordingSession.new
+  end
+
+  def create
+    recordingsession = RecordingSession.new(recordingsession_params)
+    if recordingsession.valid?
+      recordingsession.save
+      redirect_to recordingsession_path(recordingsession)
+    else
+      flash[:alert] = "Make sure to fill out all fields."
+      render :new
+    end
+  end
+
+  def show 
+  end
+
+  def edit
+  end
+  
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def recordingsession_params
+    params.require(:recordingsession).permit(:start_date, :end_date, :hours_per_day, :band?, :drums?, :genre, :budget)
+  end
+
+  def set_recordingsession
+    @recordingsession = RecordingSession.find(params[:id])
+  end
+end
