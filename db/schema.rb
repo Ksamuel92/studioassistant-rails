86935534951bc889f006725cacf4ec77946df301
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 10:55:50 -0400

    add uid and provider attribute to user and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_173707) do
+ActiveRecord::Schema.define(version: 2021_08_03_145459) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_08_02_173707) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
   add_foreign_key "recsessions", "clients"
