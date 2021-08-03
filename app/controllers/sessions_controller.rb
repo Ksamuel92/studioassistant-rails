9e531b0ddb7c8fe83aa084039d597906f8d660c6
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 11:37:27 -0400

    refactor sessions#create'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,18 @@ class SessionsController < ApplicationController
     end
   end
 
+  def new
+  end
+
   def create
     @user = User.find_by(name: params[:name])
-    return render 'new' unless @user&.authenticate(params[:password])
-
+    if @user&.authenticate(params[:password])
     sessions[:user_id] = @user.id
     redirect_to '/'
+    else
+      flash[:error] = 'Invalid Name or Password'
+      render 'new'
+    end
   end
+
 end
