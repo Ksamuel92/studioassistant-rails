2cc72eef4a8f680db10631af8e5c4c21b3263707
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 13:08:15 -0400

    delete unnecessary files

--- app/models/recsession.rb
+++
@@ -1,5 +0,0 @@
-class RecSession < ApplicationRecord
-  belongs_to :studio
-  belongs_to :client
-  validates_presence_of :start_date, :end_date, :hours_per_day, :band?, :drums?, :genre, :budget
-end
--- db/migrate/20210802164612_change_sessions_to_rec_sessions.rb
+++
@@ -1,5 +0,0 @@
-class ChangeSessionsToRecSessions < ActiveRecord::Migration[6.1]
-  def change
-    rename_table :sessions, :recsessions
-  end
-end
