0cb158ae73213759b2b2824126a395f36316cf70
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 13:06:53 -0400

    update to recordingsessions in studio model

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,5 +1,6 @@
 class Studio < ApplicationRecord
-  has_many :recsessions
+  has_many :recordingsessions
+  has_many :clients, through: :recordingsessions
   belongs_to :user
-  validates_presence_of :title, :address, :description, :rental_cost, :type
+  validates_presence_of :title, :address, :description, :rental_cost
 end
