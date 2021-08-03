1b04459887847cf445a59302218389150af0120b
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-03 17:05:45 -0400

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-
+before_action :require_login, only: [:show]
   def new
+    @user = User.new
+  end
+
+  def show
+
   end
 
   def create
@@ -10,9 +15,19 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to 'home'
     else
-      flash[:alert] = "User needs a name, password, and a unique email."
+      flash[:alert] = 'User needs a name, password, and unique email.'
       render :new
     end
+  end
+
+  def home
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
