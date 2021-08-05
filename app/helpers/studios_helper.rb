7903be8b9fb22007033dda231e4c864201e6b382
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-05 11:06:38 -0400

    add rental cost helper to StudiosHelper

--- app/helpers/studios_helper.rb
+++ app/helpers/studios_helper.rb
@@ -1,2 +1,9 @@
 module StudiosHelper
+    def rental_cost_helper(studio)
+        if studio.rental_cost = 0
+            nil
+        else
+            "Rental Cost: #{studio.rental_cost}"
+        end
+    end
 end
