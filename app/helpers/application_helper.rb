81db7be8e1dbad2f5218d46852d24df74b964c3a
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-05 14:31:35 -0400

    add application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    def current_user 
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def navigation_helper 
+        if !logged_in?
+            link_to("Sign Up", signup_path)
+            link_to("Log In", login_path)
+        else 
+            link_to("Log Out", logout_path, method: 'delete')
+        end
+    end
 end
