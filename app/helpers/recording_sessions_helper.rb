859ce5e2282a243a3b48336a11ed9bc537eee90a
Kyle Samuel <ksamuel92@gmail.com>
2021-08-10 12:28:21 -0400

    create index_header_sessions helper method for recsession

--- app/helpers/recording_sessions_helper.rb
+++ app/helpers/recording_sessions_helper.rb
@@ -2,4 +2,12 @@ module RecordingSessionsHelper
     def hours_per_day_helper(session)
         ActionController::Base.helpers.pluralize(session.hours_per_day, 'hour')
     end
+
+    def index_header_sessions
+      if params[:action] == "this_week"
+          content_tag(:h2, "This Week's Sessions")
+        else
+          content_tag(:h2, "Your Sessions")
+        end
+    end
 end
