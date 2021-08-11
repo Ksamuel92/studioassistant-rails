4c253cb01ff69c15d65bdbe6b4fc1950777465d3
Kyle Samuel <ksamuel92@gmail.com>
2021-08-11 13:31:36 -0400

    clean clienthelper

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,11 +1,18 @@
 module ClientsHelper
-    
-    def band_helper(client)
-        client.band ? content_tag(:p, "The client is bringing their own band") :content_tag(:p, "The client will not be providing musicians.")
+  
+  def band_helper(client)
+    if client.band  
+      content_tag(:p, 'The client is bringing their own band')
+    else
+      content_tag(:p, 'The client will not be providing musicians.')
     end
+  end
 
-
-    def drums_helper(client)
-        client.drums ? "The client will be bringing their own drums. Make sure to let them know to come early to set up!" : "The client will be using in house drums (if available)."
+  def drums_helper(client)
+    if client.drums  
+      'The client will be bringing their own drums. Make sure to let them know to come early to set up!'
+    else
+      'The client will be using in house drums (if available).'
     end
+  end
 end
