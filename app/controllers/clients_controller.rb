d63a11ee49bac622724ccaf417d0323bb3a65e1e
Kyle Samuel <ksamuel92@gmail.com>
2021-08-11 13:09:34 -0400

    clean recsession controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,21 +1,9 @@
 class ClientsController < ApplicationController
   before_action :set_studio, only: [:create, :new]
   def new
-    @client = Client.new
-    @client.recording_sessions.build
-    # byebug
   end
 
   def create
-    client = Client.new(client_params)
-    byebug
-    if client.valid?
-      client.save
-      redirect_to '/'
-    else
-      flash[:alert] = "Make sure to fill out all fields."
-      render :new
-    end
   end
 
   private
--- app/controllers/recording_sessions_controller.rb
+++ app/controllers/recording_sessions_controller.rb
@@ -1,26 +1,25 @@
 class RecordingSessionsController < ApplicationController
   before_action :require_login
-  before_action :set_studio, only: [:new, :create, :show, :edit, :update]
-  before_action :set_recordingsession, except: [:new, :create, :index, :thisweek]
+  before_action :set_studio, only: %i[new create show edit update]
+  before_action :set_recordingsession, except: %i[new create index thisweek]
   
   def index
-  @studios = current_user.studios
-  @recordingsessions = current_user.recording_sessions
+    @studios = current_user.studios
+    @recordingsessions = current_user.recording_sessions
   end
 
- def new
+  def new
     @recordingsession = RecordingSession.new
     @recordingsession.build_client
- end
+  end
 
   def create
     @recordingsession = RecordingSession.new(recordingsession_params)
-    # @studio = Studio.find_or_create_by(id: params[:studio_id])
     if @recordingsession.valid?
       @recordingsession.save
       redirect_to studio_recording_session_path(@recordingsession.studio, @recordingsession)
     else
-      flash[:alert] = "Make sure to fill out all fields."
+      flash[:alert] = 'Make sure to fill out all fields.'
       render :new
     end
   end
@@ -30,38 +29,38 @@ class RecordingSessionsController < ApplicationController
 
   def edit
   end
-  
+
   def update
     @recordingsession.update(recordingsession_params)
     if @recordingsession.valid?
-        flash[:notice] = "Session Sucessfully Updated"
-        redirect_to studio_recording_session_path(@recordingsession.studio, @recordingsession)
+      flash[:notice] = 'Session Sucessfully Updated'
+      redirect_to studio_recording_session_path(@recordingsession.studio, @recordingsession)
     else
-      flash[:notice] = "Invalid Update"
+      flash[:notice] = 'Invalid Update'
       render :edit
     end
   end
 
   def destroy
-    byebug
     @recordingsession.destroy
     redirect_to recording_sessions_path
   end
 
   def thisweek
-    # byebug
     @recordingsessions = current_user.recording_sessions.this_week
-    render "recording_sessions/index"
+    render 'recording_sessions/index'
   end
 
   private
 
   def recordingsession_params
-    params.require(:recording_session).permit(:id, :studio_id,
+    params.require(:recording_session).permit(
+      :id,
+      :studio_id,
       :start_date,
       :hours_per_day,
       :per_hour,
-      client_attributes: [:id, :name, :email, :genre, :budget, :band, :drums]
+      client_attributes: %i[id name email genre budget band drums]
     )
   end
 
