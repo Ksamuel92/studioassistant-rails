aafda27c0d9a15084aa47489eca67b88827d53d2
Kyle Samuel <ksamuel92@gmail.com>
2021-08-11 13:33:51 -0400

    clean recordingsessionhelper

--- app/helpers/recording_sessions_helper.rb
+++ app/helpers/recording_sessions_helper.rb
@@ -1,26 +1,26 @@
 module RecordingSessionsHelper
-    def hours_per_day_helper(session)
-        ActionController::Base.helpers.pluralize(session.hours_per_day, 'hour')
-    end
+  def hours_per_day_helper(session)
+    pluralize(session.hours_per_day, 'hour')
+  end
 
-    def index_header_sessions
-      if params[:action] == "this_week"
-          "This Week's Sessions"
-        else
-          "Your Sessions"
-        end
+  def index_header_sessions
+    if params[:action] == 'thisweek'
+      "This Week's Sessions"
+    else
+      'Your Sessions'
     end
+  end
 
-    def days_available_helper(session)
-        pluralize(session.days_available_by_budget, "day")
-    end
+  def days_available_helper(session)
+    pluralize(session.days_available_by_budget, "day")
+  end
 
-    def submit_form_helper
-      if params[:action] == "edit"
-        "Edit Session"
-      else
-        "Create Session"
-      end
+  def submit_form_helper
+    if params[:action] == 'edit'
+      'Edit Session'
+    else
+      'Create Session'
     end
+  end
 end
 
