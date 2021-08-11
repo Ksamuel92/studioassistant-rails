9f9a4925a29b65bad83d5426ac450c49967fb4fb
Kyle Samuel <ksamuel92@gmail.com>
2021-08-11 13:39:16 -0400

    clean userhelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,10 @@
 module UsersHelper
-    def sign_up_button_helper
-        if !logged_in?
-            content_tag(:div, button_to("Sign Up", signup_path, :method => :get, class: "text-center btn btn-primary btn-lg"))
-        end
+  def sign_up_button_helper
+    if !logged_in?
+      content_tag(
+        :div,
+        button_to('Sign Up', signup_path, method: :get, class: 'text-center btn btn-primary btn-lg')
+      )
+    end
     end
 end
