41cf167d7711d6e3be0d8ed87b101674ec657b87
Kyle Samuel <ksamuel92@gmail.com>
2021-08-15 17:40:00 -0400

    add before_save capitalize_attributes callback to studio model.

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -5,4 +5,16 @@ class Studio < ApplicationRecord
   belongs_to :user
   validates_presence_of :name, :address_line_1, :city, :state, :zip_code, :daw, :rental_cost
   validates :email, presence: true, uniqueness: true
+  before_save :capitalize_attributes
+
+  def capitalize_attributes
+    self.attributes.each do |attr,val|
+      self.send("#{attr}=",val.strip.titleize) if self.capitalizable_attributes.include?(attr) && !val.nil?
+    end    
+  end
+
+
+  def capitalizable_attributes
+    ["name", "address_line_1", "city", "state", "daw"]
+  end
 end
