1a6e7b67ab9662216917a02ba47c09d636a7e93b
Kyle Samuel <ksamuel92@gmail.com>
2021-08-16 14:21:45 -0400

    refactor: :art: refactor: :art: refactor before_action :check_user for recsession

--- app/controllers/recording_sessions_controller.rb
+++ app/controllers/recording_sessions_controller.rb
@@ -2,7 +2,7 @@ class RecordingSessionsController < ApplicationController
   before_action :require_login
   before_action :set_studio, only: %i[new create show edit update]
   before_action :set_recordingsession, except: %i[new create index thisweek]
-  before_action :check_user, only: [:show, :edit]
+  before_action :check_user, only: %i[show edit]
   
   def index
     @studios = current_user.studios
