ea3fe3f9737aef576abedcf77000cc61ad27f40b
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-16 10:21:03 -0400

    add content_tag to drums_helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+
     @user = User.find_by(email: params[:user][:email])
     if @user&.authenticate(params[:user][:password])
       session[:user_id] = @user.id
--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -2,17 +2,17 @@ module ClientsHelper
   
   def band_helper(client)
     if client.band  
-      content_tag(:p, 'The client is bringing their own band')
+      content_tag(:li, 'The client is bringing their own band', class: "list-group-item")
     else
-      content_tag(:p, 'The client will not be providing musicians.')
+      content_tag(:li, 'The client will not be providing musicians.', class: "list-group-item")
     end
   end
 
   def drums_helper(client)
     if client.drums  
-      'The client will be bringing their own drums. Make sure to let them know to come early to set up!'
+      content_tag(:li, 'The client will be bringing their own drums. Make sure to let them know to come early to set up!', class: "list-group-item")
     else
-      'The client will be using in house drums (if available).'
+      content_tag(:li, 'The client will be using in house drums (if available).', class: "list-group-item")
     end
   end
 end
