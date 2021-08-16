314508ad15e9b02128267a34c8e08c07db9d9901
Kyle Samuel <Ksamuel92@gmail.com>
2021-08-16 11:12:58 -0400

    handle recordnotfound error with 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  
+rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+    
+
   def require_login
     unless current_user
       redirect_to '/'
@@ -9,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @user = User.find_by(id: session[:user_id])
+  end
+
+  private
+  def record_not_found
+    render plain: "404 Not Found", status: 404
   end
 end
